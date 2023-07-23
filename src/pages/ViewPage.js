5d0979965f542544093fb42c1f6138c2b2c2c841
JediKunnow <filippo.sofi.res@gmail.com>
2023-07-23 05:01:53 +0200

    remove padding from main view

--- src/pages/ViewPage.js
+++ src/pages/ViewPage.js
@@ -37,10 +37,6 @@ export default function ViewPage(props) {
       <div className="row">
         <div
           className="d-inline-block position-relative overflow-hidden"
-          style={{
-            "--body-top-padding": "24px",
-            paddingTop: "var(--body-top-padding)",
-          }}
         >
           <Widget key={src} src={src} props={widgetProps} />{" "}
         </div>
